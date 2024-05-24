e689731cb579ebb48fec558bea9a438e3ffb9fe2
Grace Kim <gracek1459@gmail.com>
2024-05-24 14:45:32 +0100

    data is downloaded

--- slrum_interact.sh
+++ slrum_interact.sh
@@ -2,5 +2,5 @@
 
 source /nfs-share/grk27/miniconda3/bin/activate fed_sats
 
-srun -w mauao -c 10 --gres=gpu:1 --partition=interactive bash 
+srun -w mauao -c 10 --gres=gpu:1 --partition=interactive --pty bash 
 srun -w ruapehu -c 10 --gres=gpu:0 --partition=interactive --pty bash
