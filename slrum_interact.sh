10ff6d369ead0957ac25c0e2cfdaa849f87af201
Grace Kim <gracek1459@gmail.com>
2025-07-07 16:22:47 -0700

    deleting slrum files

--- slrum_interact.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source /nfs-share/grk27/miniconda3/bin/activate fed_sats
-
-srun -w mauao -c 10 --gres=gpu:1 --partition=interactive --pty bash 
-srun -w ruapehu -c 20 --gres=gpu:0 --partition=interactive --pty bash
-
-ngongotaha
